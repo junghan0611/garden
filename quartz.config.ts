f33960c5281cae70e8f28ed963dadc8026782f29
junghan0611 <31724164+junghan0611@users.noreply.github.com>
2025-06-27 10:07:44 +0900

    upmorning

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,7 +19,7 @@ const config: QuartzConfig = {
     // locale: "en-US",
     locale: "ko-KR",
     baseUrl: "notes.junghanacs.com",
-    ignorePatterns: ["private", "tmp", "templates", ".obsidian"],
+    ignorePatterns: ["private", "office", "tmp", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
